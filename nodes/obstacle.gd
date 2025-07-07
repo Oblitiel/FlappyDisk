9115fa0d1a107bd7609f132a00b4739864516285
Gabriel González <oblitiel@gmail.com>
2025-07-07 22:32:25 +0200

    Movable Obstacle

--- nodes/obstacle.gd
+++ nodes/obstacle.gd
@@ -1,7 +1,7 @@
 class_name Obstacle
 extends Area2D
 
-signal player_entered()
+signal player_obstacle_collision()
 
 func _ready():
 	_connect_body_entered_from_area2d()
@@ -9,8 +9,7 @@ func _ready():
 func _connect_body_entered_from_area2d() -> void:
 	body_entered.connect(_on_body_entered)
 
-
-
 func _on_body_entered(body : Node2D) -> void:
 	if body is FlappyCharacter:
 		GameController.instance.on_player_obstacle_collision()
+		player_obstacle_collision.emit()
